c92c4229d8962c021b746cf6ae678855819181d5
SciLor <bh44l@gmx.de>
2016-03-12 17:31:38 +0100

    Removed unused LOGLEVEL

--- BaseHeader.h
+++ BaseHeader.h
@@ -5,10 +5,6 @@
 #include <Logging.h>
 #include "Config.h"
 
-#define LOGLEVEL LOG_LEVEL_INFOS
-//#define LOGLEVEL LOG_LEVEL_DEBUG
-//#define LOGLEVEL LOG_LEVEL_VERBOSE
-
 #define min(a,b) ((a)<(b)?(a):(b))
 
 #endif

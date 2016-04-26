51af94aa4d8326c8647fa242f25e71695249ba6f
adn77 <alexander.noack@web.de>
2016-04-26 14:00:13 +0200

    Add NeoPixelBus as an alternative to FastLED
    Added config option HW_FASTLED / HW_NEOPIXEL in order to replace FastLED hardware library with NeoPixelBus (https://github.com/Makuna/NeoPixelBus) for clockless strips (like WS2812B).
    FastLED currently only supports bit banging while NeoPixelBus allows for DMA or UART interface which in turn should lead to a more stable network connection.

--- WrapperFastLed.h
+++ WrapperFastLed.h
@@ -2,7 +2,14 @@
 #define WrapperFastLed_h
 
 #include "BaseHeader.h"
-#include <FastLED.h>
+#ifdef HW_FASTLED
+  #include <FastLED.h>
+#elif HW_NEOPIXEL
+  #include <NeoPixelBus.h>
+#endif
+
+#define colorSaturation CONFIG_MAX_COLOR
+#define colorFactor 255 / CONFIG_MAX_COLOR
 
 class WrapperFastLed {
   public:
@@ -14,19 +21,32 @@ class WrapperFastLed {
       #endif
       show(void),
       clear(void),
-      fillSolid(CRGB color),
+      #ifdef HW_FASTLED
+        fillSolid(CRGB color),
+      #elif HW_NEOPIXEL
+        setPixel(byte i, byte r, byte g, byte b),
+        fillSolid(RgbColor color),
+      #endif
       fillSolid(byte r, byte g, byte b),
       rainbowStep(void);
 
-    CRGB* leds;
+    #ifdef HW_FASTLED
+      CRGB* leds;
+    #elif HW_NEOPIXEL
+      typedef NeoPixelBus<CONFIG_NEO_FEATURE, CONFIG_NEO_METHOD> MyPixelBus;
+      MyPixelBus * strip;
+    #endif
       
   private: 
-   #ifdef CONFIG_TYPE_WEBCONFIG
-    void 
-        addLeds(uint8_t chipset, uint8_t dataPin, uint8_t clockPin, uint8_t colorOrder, uint16_t ledCount);
-   #endif
-      
-    CRGB wheel(byte wheelPos);
+    #ifdef HW_FASTLED
+      #ifdef CONFIG_TYPE_WEBCONFIG
+       void 
+         addLeds(uint8_t chipset, uint8_t dataPin, uint8_t clockPin, uint8_t colorOrder, uint16_t ledCount);
+      #endif
+      CRGB wheel(byte wheelPos);
+    #elif HW_NEOPIXEL
+      RgbColor wheel(byte wheelPos);
+    #endif
     byte _rainbowStepState;
     int _ledCount;
 };

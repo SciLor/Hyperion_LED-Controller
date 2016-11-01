be2295845f8ea760bf3a3d246137665d060a12d6
SciLor <bh44l@gmx.de>
2016-11-01 10:23:15 +0100

    Fixed wrong caption of Fire2012

--- WrapperWebconfig.cpp
+++ WrapperWebconfig.cpp
@@ -331,5 +331,5 @@ void WrapperWebconfig::getIdleModes(uint8_t active, LinkedList<SelectEntryBase*>
   target->add((SelectEntryBase*) new SelectEntry<uint8_t>("Off", "Off", active == OFF, OFF));
   target->add((SelectEntryBase*) new SelectEntry<uint8_t>("Static", "Static color", active == STATIC_COLOR, STATIC_COLOR));
   target->add((SelectEntryBase*) new SelectEntry<uint8_t>("Rainbow", "Rainbow", active == RAINBOW, RAINBOW));
-  target->add((SelectEntryBase*) new SelectEntry<uint8_t>("Fire2012", "Rainbow", active == FIRE2012, FIRE2012));
+  target->add((SelectEntryBase*) new SelectEntry<uint8_t>("Fire2012", "Fire2012", active == FIRE2012, FIRE2012));
 }
